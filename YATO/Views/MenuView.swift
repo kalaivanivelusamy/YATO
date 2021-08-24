991719e763c918f9d56b32eaf2323881060a60b4
KalaivaniVelusamy <kalai.v.vani@gmail.com>
2021-08-24 21:51:47 +0530

    added profile image and changed the chart swift ui swift package manager

--- YATO/Views/MenuView.swift
+++ YATO/Views/MenuView.swift
@@ -1,11 +1,28 @@
 import SwiftUI
-import SwiftUICharts
+import Charts
 
 struct MenuView: View {
     
 
     var body: some View {
         VStack(alignment: .leading) {
+            
+            VStack(alignment: .center,spacing:10) {
+                Image(uiImage: UIImage(named: "Profile")!)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 100, height: 100, alignment: .center)
+                    
+                    .clipShape(Circle())
+                    .shadow(radius: 10)
+                    .overlay(Circle().stroke(Color.red,lineWidth: 2))
+                Text("Name")
+                    .foregroundColor(.white)
+                    .font(.title)
+            }.padding(.top,200)
+           
+            Spacer()
+
             HStack {
                 Image(systemName: "person")
                     .foregroundColor(.gray)
@@ -13,7 +30,7 @@ struct MenuView: View {
                 Text("Profile")
                     .foregroundColor(.gray)
                     .font(.headline)
-            }.padding(.top,100)
+            }.padding(.top,30)
             
             HStack {
                     Image(systemName: "envelope")
@@ -36,23 +53,26 @@ struct MenuView: View {
            
             Spacer()
             
-            VStack {
+            VStack(alignment: .leading, spacing: 5) {
                //Graph for consistency
-                LineView(data: [8,23,54,32,12,37,7,23,43])
-                    
-                    .padding(20)
+                
+                Chart(data: [0.7,0.1,0.3,0.5,0.6])
+                    .chartStyle(
+                        LineChartStyle(.quadCurve, lineColor: .blue, lineWidth: 3.0))
+                    .shadow(radius: 3)
+                    .frame(width: 200, height: 100, alignment: .leading)
 
                 Text("Good")
                     .foregroundColor(.gray)
                     .font(.headline)
-                
+                                
                 Text("Consistency")
                     .foregroundColor(.white)
                     .font(.title2)
-            }.padding(.bottom,30)
+            }.padding(.bottom,50)
             
         }
-        .padding()
+        .padding(.leading,30)
         .frame(maxWidth: .infinity, alignment: .leading)
         .background(Color(red: 32/255, green: 32/255, blue: 32/255))
         .edgesIgnoringSafeArea(.all)
